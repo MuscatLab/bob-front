2415002830063e55a2f7167f7a71a0fafcbdccb6
SeryeongK <99seryoung@naver.com>
2023-08-20 10:53:25 +0900

    (style) Change font size

--- src/app/order/result/page.tsx
+++ src/app/order/result/page.tsx
@@ -13,10 +13,10 @@ const Result = ({
     <div className="flex bg-black justify-center items-center w-screen h-screen">
       <div className="w-[550px] h-full bg-white text-black flex flex-col">
         <section id="order" className="bg-white h-full mx-4">
-          <h1 className="text-4xl mt-60">
-            Thank You<span className="text-3xl block">for Purchasing!</span>
+          <h1 className="text-5xl mt-60">
+            Thank You<span className="text-4xl block">for Purchasing!</span>
           </h1>
-          <p className="text-3xl mt-8">
+          <p className="text-4xl mt-8">
             Your order No. is
             <br />
             <span className="text-6xl textSecondary text-[#F75E28]">
