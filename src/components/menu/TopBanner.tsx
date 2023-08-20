26e941738c5c401e005bd618079fb838015982da
SeryeongK <99seryoung@naver.com>
2023-08-20 09:32:13 +0900

    (style) Add cuprice image

--- src/components/menu/TopBanner.tsx
+++ src/components/menu/TopBanner.tsx
@@ -1,16 +1,16 @@
 const TopBanner = () => {
   return (
-    <div className="w-full h-1/6 flex-shrink-0 bg-gradient-to-r from-red-700 to-black flex items-center pl-3">
-      <div>
+    <div className="w-full h-1/6 flex-shrink-0 bg-gradient-to-r from-red-700 to-black flex items-center pl-3 justify-between">
+      <div className="px-4">
         <p
-          className="text-4xl text-white"
+          className="text-3xl text-white"
           style={{ textShadow: "0px 8px 15px rgba(0, 0, 0, 0.70)" }}
         >
           Emm...
         </p>
         <div className="flex items-center">
           <span
-            className="text-white text-5xl font-normal"
+            className="text-white text-4xl font-normal"
             style={{ textShadow: "0px 8px 15px rgba(0, 0, 0, 0.70)" }}
           >
             What&#39;s up,
@@ -29,6 +29,7 @@ const TopBanner = () => {
           </span>
         </div>
       </div>
+      <img src="/CupRice.png" className="w-[9.9rem]" />
     </div>
   );
 };
